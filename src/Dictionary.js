757773d7911267c61cb50c68b075bd690a153cee
KatjaArndt <80687991+KatjaArndt@users.noreply.github.com>
2021-06-20 22:13:33 +0200

    Meaning, definition, example

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,19 @@
 import React, {useState} from "react";
 import axios from "axios";
-import './dictionary.css';
+import "./dictionary.css";
+import Results from "./Results.js";
 
 export default function Dictionary (){
 let [keyword, setKeyword] =useState("");
+let [results, setResults] =useState(null);
 function handleResponse(response){
-    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
 }
 
 function search (event){
         event.preventDefault();
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        console.log(apiUrl);
         axios.get(apiUrl).then(handleResponse);
 }
         
@@ -26,6 +28,7 @@ function handleKeywordChange(event){
 
          
         </form>
+        <Results results = {results}/>
     </div>
 }
 
