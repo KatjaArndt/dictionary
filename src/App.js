cf2a5903680562c50aebcfa6e75173be853dcac6
KatjaArndt <80687991+KatjaArndt@users.noreply.github.com>
2021-06-20 12:56:01 +0200

    created structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import Dictionary from './Dictionary.js';
+
+
 
 function App() {
   return (
     <div className="App">
+      <div className ="container">
       <header className="App-header">
         <p>
           Learn React
           </p>
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className ="center">
+        Coded by Katja Arndt
+      </footer>
+      </div>
     </div>
   );
 }
